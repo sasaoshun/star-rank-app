d256fac209748052df5bc4d5b9780b998a2949a9
sasao <sasaoshun@gmail.com>
2023-01-03 14:24:17 +0100

    upload

--- app.py
+++ app.py
@@ -2,16 +2,23 @@ from flask import Flask, render_template
 import git 
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./templates/img')
 
 @app.route('/')
 def scraping_api():
-    return render_template('index.html')
+    back_pic = './img/background.png'
+    img_path = './img/star.png'
+    return render_template('index.html', img_path = img_path, back_pic = back_pic)
 
 @app.route('/results', methods=['POST'])
 def do_scraping():
+    univers_img = './img/univers.png'
     results = git.git_api()
-    return render_template('results.html', results=results)
+    urls = git.link_url()
+    
+    return render_template('results.html', univers_img = univers_img, git_links=zip(results,urls))
+
 
 if __name__ == ('__main__'):
-    app.run()
+    app.run(debug=True)
+  
--- git.py
+++ git.py
@@ -8,21 +8,42 @@ from flask import request
 def git_api():
         serchfor = request.form.get('rank_git')
         serchfor = str(serchfor)
-        url = 'https://api.github.com/search/repositories?q=stars%3A%3E1+created%3A' + serchfor + '&type=Repositories&ref=advsearch&l=&l='
+        serchfor_language = request.form.get('rank_lauguage')
+        url = 'https://api.github.com/search/repositories?q=stars%3A%3E1+created%3A' + serchfor + '+language%3A'+ serchfor_language + '&type=Repositories&ref=advsearch&l=&l='
         response = requests.get(url)
         data = json.loads(response.text)
         api_lists = []
         i = 0
         while i < 5:
             
-            ranks = [['第' + str(i + 1) + '位   ユーザー名: ' + data.get('items')[i].get('name')], ['スター獲得数:  ' + str(data.get('items')[i].get('stargazers_count'))]]
-            time.sleep(3)
+            ranks = [['第' + str(i + 1) + '位   ユーザー名: ' + data.get('items')[i].get('name')],['スター獲得数:  ' + str(data.get('items')[i].get('stargazers_count'))]]
+            time.sleep(2)
             i += 1
             ranks = str(ranks).replace('[','').replace(']','')
             ranks = str(ranks).replace('\'','').replace('\'','')
-            api_lists.append(ranks)
-            
+            api_lists.append(ranks)  
         return api_lists
+    
+def link_url():
+        serchfor = request.form.get('rank_git')
+        serchfor = str(serchfor)
+        serchfor_language = request.form.get('rank_lauguage')
+        url = 'https://api.github.com/search/repositories?q=stars%3A%3E1+created%3A' + serchfor + '+language%3A'+ serchfor_language + '&type=Repositories&ref=advsearch&l=&l='
+        response = requests.get(url)
+        data = json.loads(response.text)
+        api_url = []
+        
+        i = 0
+        while i < 5:
+            
+            url_links =[[str(data.get('items')[i].get('html_url'))]]
+            time.sleep(2)
+            i += 1
+            url_links = str(url_links).replace('[','').replace(']','')
+            url_links = str(url_links).replace('\'','').replace('\'','')
+            api_url.append(url_links)
+        return api_url
+            
 
 
   
